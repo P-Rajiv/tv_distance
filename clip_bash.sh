170f5d814a0b7ccdbb527bd261911588f1f44d75
Rajiv P <rajiv.porana@gmail.com>
2024-12-27 16:32:22 +0530

    clip_bash added

---
+++ clip_bash.sh
@@ -0,0 +1,1 @@
+python3 -W ignore libs/bin_tv.py --path1 /data6/rajivporana_scratch/chexphoto_data/CheXphoto-v1.0/train_data/wh_proper_labelled_train  --batch_size 128 --device cuda:3 --save_path chexphoto_train
